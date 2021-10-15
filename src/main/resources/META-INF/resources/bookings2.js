9dcb01d8eaddc57a7bc95782e665020d8e6bf2c8
Phillip Kruger <phillip.kruger@gmail.com>
2021-10-15 16:48:13 +0200

    added style var override Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/resources/META-INF/resources/bookings2.js
+++ src/main/resources/META-INF/resources/bookings2.js
@@ -1,11 +1,18 @@
 import {graphQLRequest} from '/graphql.js';
 
-import {LitElement, html} from 'https://unpkg.com/lit@2.0.0/index.js?module';
-import {until} from 'https://unpkg.com/lit@2.0.0/directives/until.js?module';
+import {LitElement, html} from 'https://unpkg.com/lit@2.0.2/index.js?module';
+import {until} from 'https://unpkg.com/lit@2.0.2/directives/until.js?module';
 
 import 'http://localhost:9090/member.js';
 
 export class BookingTable extends LitElement {
+    
+    static styles = css`
+        table, td, th {
+            border-bottom: 1pt solid var(--table-border-color, black);
+            border-collapse: collapse;
+        }
+    `;
     
     static properties = {
         fields: {type: String},
